c3ed63ffc5c9c6fd689f2ac6c108f870299b8afc
Nexssus <mnegovan1979@gmail.com>
2022-11-05 21:56:35 +0100

    Register new user, create an account

--- test/mystoreqa.js.js
+++ test/mystoreqa.js.js
@@ -25,5 +25,37 @@ describe('swagLabs', function() {
         // Pokrece se nakon svakog testa
     });
 
-    
+    it("Verify homepage is open", async function(){
+        await driver.get('http://automationpractice.com/index.php');
+        expect(await driver.getCurrentUrl()).to.eq("http://automationpractice.com/index.php");
+    });
+
+    it("Goes to registration page", async function(){
+        const sign = await driver.findElement(By.className('login'));
+        await sign.click();
+        expect(await driver.findElement(By.className('page-heading')).getText()).to.contain('AUTHENTICATION');
+    })
+
+    it("Create an account", async function(){
+        const register = await driver.findElement(By.id('email_create'));
+        register.sendKeys('mnegovan@example.com');
+        const confirmBtn = await driver.findElement(By.id('SubmitCreate'));
+        await confirmBtn.click();
+        expect(await driver.findElement(By.className('page-subheading')));
+    })
+
+    it("Input personal information", async function(){
+        //const gender = await driver.findElement(By.id('id_gender1'));
+       // await gender.click();
+        const firstName = await driver.findElement(By.id('customer_firstname'));
+        firstName.sendKeys('Negovan');
+        const lastName = await driver.findElement(By.id('customer_lastname'));
+        lastName.sendKeys('Milenkovic');
+        const email = await driver.findElement(By.id('email'));
+        email.sendKeys('mnegovan@example.com');
+        const password = await driver.findElement(By.id('passwd'));
+        password.sendKeys(12345);
+
+    })
+
 });    
